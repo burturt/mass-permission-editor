a1f44e13ce46ace26b7ebcd8e113e9a83bf4437a
burturt <31748545+burturt@users.noreply.github.com>
2020-12-20 11:00:26 -0800

    Make script look better

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import os
 import discord
 from dotenv import load_dotenv
-from discord.utils import get
 from discord.ext import commands
 
 load_dotenv()
@@ -96,4 +92,3 @@ async def clearperms(ctx):
 
 bot.run(TOKEN)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
